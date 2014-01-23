b2f63de10c9e28808d668f3a4973cad87b4670c6
Tom Nguyen <tomng@cs.stanford.edu>
2014-01-22 21:53:25 -0800

    Added animated photo wall, based on handlebars and jquery.

--- js/inforum.js
+++ js/inforum.js
@@ -1,3 +1,25 @@
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 $.get( "https://graph.facebook.com/INFORUMsf/albums", function (albums) {
 
   for ( var albumIndex = 0; albumIndex < 3; albumIndex++ ) {
@@ -15,5 +37,184 @@ $.get( "https://graph.facebook.com/INFORUMsf/albums", function (albums) {
 
   }
 
+  // Handlebars templating for flip tiles
+
+  var fliptileData = [
+	  	{
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Alexis Ohanian",
+		  	speakerDetail: "Founder of Reddit",
+		  	programTitle: "Ask Me Anything Live",
+		  },
+		  {
+		  	imageFilename: "head-shot.jpg",
+		  	speakerName: "Rachel Maddow",
+		  	speakerDetail: "TV Journalist",
+		  	programTitle: "A Conversation with Rachel Maddow",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 3",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 4",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 5",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 6",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 7",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 8",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 9",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 10",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 11",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 12",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 13",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 13",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 14",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 15",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 16",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 17",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 18",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 19",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 20",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 21",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },
+		  {
+		  	imageFilename:  "head-shot.jpg",
+		  	speakerName: "Speaker 22",
+		  	speakerDetail: "Speaker Detail",
+		  	programTitle: "Program Title",
+		  },		  
+
+	  ];
+
+  var source = $("#fliptile-template").html();
+  var template = Handlebars.compile(source);
+
+  var fliptileDataShuffled = shuffle(fliptileData);
+
+  for (var i = 0; i < fliptileDataShuffled.length; i++) {
+	  $("#tile" + i).append(template(fliptileDataShuffled[i]));
+  };
+
+  // Flip animation
+
+  if ($('html').hasClass('csstransforms3d')) {  
+  
+    $('.thumb').removeClass('scroll').addClass('flip');   
+    $('.thumb.flip').hover(
+      function () {
+        $(this).find('.thumb-wrapper').addClass('flipIt');
+      },
+      function () {
+        $(this).find('.thumb-wrapper').removeClass('flipIt');     
+      }
+    );
+    
+  } else {
+
+    $('.thumb').hover(
+      function () {
+        $(this).find('.thumb-detail').stop().animate({bottom:0}, 500, 'easeOutCubic');
+      },
+      function () {
+        $(this).find('.thumb-detail').stop().animate({bottom: ($(this).height() * -1) }, 500, 'easeOutCubic');      
+      }
+    );
+
+  }
 
 });
