67e492a5bd324a20fd225fe3f1963123bfa3c4e7
Tom Nguyen <tomng@cs.stanford.edu>
2014-03-26 23:03:22 -0700

    Text and CSS tweaks

--- js/inforum.js
+++ js/inforum.js
@@ -72,8 +72,8 @@ $(function(){
 
 $(document).ready(function(){
   if(window.innerWidth > 992) {
-    $('.stay-informed').parallax("50%", 0.1);
-    $('.description').parallax("50%", 0.1);
-    $('.quotes').parallax("50%", 0.1);
+    $('.stay-informed').parallax("50%", 0.7);
+    $('.description').parallax("50%", 0.7);
+    $('.quotes').parallax("50%", 0.7);
   }
 });
