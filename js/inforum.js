9a642404fb66c88fdb7575c55247a37d9cddfca8
courtlandalves <courtlandalves@gmail.com>
2014-10-26 01:15:12 -0700

    getting even closer

--- js/inforum.js
+++ js/inforum.js
@@ -1,29 +1,3 @@
-// Adding facebook album photos to event section
-$.get( "https://graph.facebook.com/INFORUMsf/albums", function (albums) {
-
-  for ( var albumIndex = 0; albumIndex < albums.data.length; albumIndex++ ) {
-    var album = albums.data[albumIndex];
-
-    var photosToReplace = 6;
-    var photosReplaced = 0;
-
-    if ( album.cover_photo && album.name != "Cover Photos" && album.name != "Timeline Photos" && album.name != "Profile Pictures") {
-
-      $.get("https://graph.facebook.com/" + album.cover_photo, function (photo) {
-        
-        var ratio = (photo.width + 0.0) / photo.height;
-        if ( photosReplaced < photosToReplace && Math.abs(ratio - 1.5) < .1 ) { // Making sure that ratio is close to a size we like
-          $("#event_" + photosReplaced).attr("src", photo.source);
-          photosReplaced++;
-        }
-
-      });
-
-    }
-
-  }
-});
-
 function renderGoogleData(data, tabletop) {
 
   renderUpcoming(data.upcoming);
@@ -36,7 +10,9 @@ function renderUpcoming(upcoming) {
   var source = $("#upcoming-template").html();
   var template = Handlebars.compile(source);
 
-  for (var i = 0; i < upcoming.elements.length; i++) {
+  var photosToReplace = 3;
+
+  for (var i = 0; i < photosToReplace; i++) {
     $("#upcoming-items").append( template(upcoming.elements[i]) );
   }
   
@@ -52,6 +28,7 @@ function renderRecent(recent) {
   }
   
 }
+
 $( function() {
   Tabletop.init( { 
     key: '0Arjh_9mskXPsdEJTcDduN1hhT053TGpTQWdDZVJVclE',
